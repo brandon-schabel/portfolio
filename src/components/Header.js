96e0d13dce50b507e4031d4c03b9e18729d008fa
brandon-schabel <brandonschabel1995@gmail.com>
2018-01-07 19:02:49 -0800

    move navbar to app

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,35 +5,7 @@ class Header extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-default navbar-static-top">
-                    <ul className="nav nav-justified">
-                        <center>
-                            <li>
-                                <Link to="/">
-                                    <b>Home</b>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/about">
-                                    <b>About</b>
-                                </Link>
-                            </li>
-                            <li>
-                                <a
-                                    styles="text-decoration: none;"
-                                    href="./sourceFiles/Resume.pdf"
-                                    className="profileSubHeading">
-                                    <b>Resume</b>
-                                </a>
-                            </li>
-                            <li>
-                                <Link to="/portfolio">
-                                    <b>Portfolio</b>
-                                </Link>
-                            </li>
-                        </center>
-                    </ul>
-                </nav>
+                
             </div>
         );
     }
