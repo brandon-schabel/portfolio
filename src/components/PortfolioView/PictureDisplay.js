7b3daff106b1772a0f90b05d17afbdba27753b32
brandon-schabel <brandonschabel1995@gmail.com>
2018-01-11 18:12:32 -0800

    add lazy load, convert PictureDisplay to functional component

--- src/components/PortfolioView/PictureDisplay.js
+++ src/components/PortfolioView/PictureDisplay.js
@@ -1,23 +1,24 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './PictureDisplay.css';
 // change to functional component
-import {Image, Transformation } from 'cloudinary-react';
+import {Image, Transformation} from 'cloudinary-react';
+import LazyLoad from 'react-lazy-load'
 
-class Picture extends Component {
-    render() {
-        return (
-            <div className="col-md-4 pictureDisplay">
-                <Image key={Math.rand} publicId={this.props.publicId}>
+const Picture = (props) => {
+    return (
+        <div className="col-md-4 pictureDisplay">
+            <LazyLoad height={200}>
+                <Image key={Math.rand} publicId={props.publicId}>
                     <Transformation
-                    crop="scale"
-                    width = "300"
-                    height = "200"
-                    dpr = "auto"
-                    responsive_placeholder = "blank"/>
+                        crop="scale"
+                        width="300"
+                        height="200"
+                        dpr="auto"
+                        responsive_placeholder="blank"/>
                 </Image>
-            </div>
-        );
-    }
+            </LazyLoad>
+        </div>
+    );
 }
 
 export default Picture;
