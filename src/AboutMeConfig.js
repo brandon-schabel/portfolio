4119665d1d904d290e9b9557e00f779a5a28a158
brandon-schabel <brandonschabel1995@gmail.com>
2018-01-19 22:42:33 -0800

    add about me and resume config files

---
+++ src/AboutMeConfig.js
@@ -0,0 +1,44 @@
+const AboutMeConfig = {
+    AboutMe: "Lorem ipsum dolor sit amet, saperet necessitatibus cu sit, ut nonumy senserit ph" +
+            "ilosophia eum. Alii dicit ullamcorper nec ea, nam eu probo minim dissentiunt, de" +
+            "nique oportere definitionem at cum. Nullam expetenda cum no, at cum nostrum fore" +
+            "nsibus, et prima reque sanctus his. Mel atqui numquam iracundia at, pri consul d" +
+            "eleniti in. Sed enim fabulas ex, ad solum quaeque quaerendum nam. Pro nemore per" +
+            "iculis reprimique te, ne sit quot ullamcorper. Sea vero impetus partiendo eu. Pr" +
+            "o ut porro harum laboramus. Pro cu commodo voluptaria efficiantur. Ut pro amet a" +
+            "ssentior, dico homero atomorum ad quo. Nisl quaerendum mei cu, qui ex aliquam im" +
+            "perdiet neglegentur, tantas doctus eu sit. At tation explicari vix, te esse vidi" +
+            "t his. Quo ei ullum pertinacia constituto, per solet verear moderatius ex, quas " +
+            "dolorem vis et. Nam in recusabo voluptatum adversarium, dicunt recteque percipit" +
+            "ur quo eu. Sea agam inciderint ei, liber torquatos mel in. Quo dolore tincidunt " +
+            "eloquentiam ne, id eos laboramus intellegat sadipscing. Sint interesset ea duo. " +
+            "Impedit quaestio an vel. Cu tamquam delectus vituperatoribus sea, eu sed duis es" +
+            "sent commune. Et quo tota scripta qualisque, duo ex suas fugit mediocritatem. No" +
+            " cum volutpat neglegentur, tollit causae ne mel, no vel fabulas utroque. Ius nom" +
+            "inati deterruisset cu, diam consequuntur pro eu. Eruditi civibus mediocrem te eu" +
+            "m. Te dicunt iuvaret consequat eam, populo urbanitas id vix. Ei posse civibus ve" +
+            "l, primis denique an ius. Ex mea congue audire nostrud, animal regione pericula " +
+            "pro ei, ne vitae blandit forensibus ius. Ut nonumy accusam honestatis eam, mei a" +
+            "n graece albucius. Tritani aliquid pericula qui te. Ex ridens fuisset qui, mea c" +
+            "horo vocibus nominavi et. Ullum graeco ea sea, quo decore verear no, timeam anci" +
+            "llae pertinacia eu ius. Ceteros placerat ius ut. Veri posse conceptam pro id, vo" +
+            "lumus temporibus in usu. Ius ei possim liberavisse, pro adipisci quaestio reform" +
+            "idans no. Mutat audiam quo te, saperet indoctum no duo, oratio putant disputando" +
+            " no mea. Id quo idque malorum elaboraret. Mea ridens senserit percipitur et, quo" +
+            " velit facilis blandit te, ius cu splendide moderatius dissentias. Principes eff" +
+            "iciendi voluptatibus id nam. Deleniti erroribus vix ei. Wisi dissentias scribent" +
+            "ur ut his, pri ad veri noluisse, mei accusamus splendide ex. An sit debitis medi" +
+            "ocrem. Id copiosae maluisset usu, ut mel adhuc utinam integre, et quas solum dem" +
+            "ocritum nam. Et per adhuc semper, eos eius novum assueverit no. Quidam malorum v" +
+            "ix ad. Ex populo voluptua conclusionemque nec, mel in dolorum dolorem, quo omniu" +
+            "m volumus percipit ex. Probo electram principes nec ne, est eu albucius perfecto" +
+            ". Ut quo posse accommodare, usu ne illum eligendi. Sint intellegam ne cum, dolor" +
+            " nominavi ei his, usu ad modus facete maluisset. Et mei vero rebum partem, eu at" +
+            "qui simul eam, indoctum convenire ut cum. Dicta appareat voluptatum in mel, nec " +
+            "duis falli in. Stet inciderint vix at, per eu enim dicunt commodo. Ne nibh perti" +
+            "nacia eos. Libris facilisi constituam eos ex, convenire explicari ad vel, eos ei" +
+            " saepe vidisse. Tantas deserunt eam et. Quo te affert ullamcorper. Ut debet saep" +
+            "e efficiantur est, aeque labores molestie mel in."
+}
+
+export default AboutMeConfig;
---
+++ src/components/ResumeView/ResumeConfig.js
@@ -0,0 +1,38 @@
+const ResumeConfig = {
+
+    resume: {
+        school: 'University of Example',
+        degree: 'BS in Exampling',
+        gpa: '1.5',
+        projectedCourseWork: [
+            'ex 1',
+            'ex 2',
+            'ex 3',
+            'ex 4',
+            'ex 5',
+            'ex 6',
+        ],
+        skills: [
+            'Example skill 1', 'Example skill 2', 'Example skill 3'
+        ],
+        applicableProjects: [
+            'Project 1', 'Project 2', 'Project 3'
+        ],
+        workExperience: [
+            {
+                titleAndBasicInfo: 'job title',
+                duties: [
+                    'duty 1', 'duty 2', 'duty 3'
+                ]
+            }
+        ],
+        involvementVolunteering: [
+            'ex 1', 'ex 2', 'ex 3'
+        ],
+        interest: [
+            'interest 1', 'interest 2', 'interest 3' 
+        ]
+    }
+}
+
+export default ResumeConfig;
